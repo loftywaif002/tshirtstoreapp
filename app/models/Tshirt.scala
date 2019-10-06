83dc90530781babfc50d5b47c4c04ac29dcf782d
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2019-10-06 02:17:34 -0700

    object class added

--- app/models/Tshirt.scala
+++ app/models/Tshirt.scala
@@ -1,10 +1,28 @@
 package models
 
-class Tshirt(var id: Int, var color: String, var picture: String, var price: String) {
+class Tshirt(var id: Int, var color: String, var pic_url: String, var price: Int) {
+
   var tShirts: Set[Tshirt] = Set()
 
   def add(tshirt: Tshirt): Unit ={
     tShirts+=tshirt
   }
 
+  def getTshirts(): Set[Tshirt] ={
+    return tShirts
+  }
+
+
+}
+
+// singleton object
+object Tshirt {
+  var tshirts = new Tshirt()
+  tshirts.add(new Tshirt(1, "red", "https://live.staticflickr.com/5246/5276588069_ce5ab13b09.jpg", 20))
+  tshirts.add(new Tshirt(2, "yellow", "https://live.staticflickr.com/654/21474568892_0369bd9f43.jpg", 25))
+  tshirts.add(new Tshirt(3, "blue", "https://live.staticflickr.com/8086/8454636463_8ff5034d2d.jpg", 30))
+
+  def getAllTshirts(): Set[Tshirt] ={
+    return tshirts.getTshirts()
+  }
 }
