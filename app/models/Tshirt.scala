b7422183abae130bfa5c4aebd211cb7570285ea7
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2021-08-02 00:39:30 -0700

    pic urls updated

--- app/models/Tshirt.scala
+++ app/models/Tshirt.scala
@@ -29,8 +29,8 @@ class Tshirt(var id: Int = 0, var color: String = "", var pic_url: String = "", 
 object Tshirt {
   var tshirts = new Tshirt()
   tshirts.add(new Tshirt(1, "red", "https://live.staticflickr.com/5246/5276588069_ce5ab13b09.jpg", 20))
-  tshirts.add(new Tshirt(2, "yellow", "https://live.staticflickr.com/654/21474568892_0369bd9f43.jpg", 25))
-  tshirts.add(new Tshirt(3, "blue", "https://live.staticflickr.com/8086/8454636463_8ff5034d2d.jpg", 30))
+  tshirts.add(new Tshirt(2, "yellow", "https://live.staticflickr.com/65535/51352604889_8dec34bea2.jpg", 25))
+  tshirts.add(new Tshirt(3, "blue", "https://live.staticflickr.com/65535/51352604879_b0a113f3d8_h.jpg", 30))
 
   def getAllTshirts(): Set[Tshirt] ={
     return tshirts.getTshirts()
